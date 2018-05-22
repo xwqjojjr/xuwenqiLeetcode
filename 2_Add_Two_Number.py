6e137a6a14311f0520725565e8bb179cd1296a6e
xwqjojjr <xuwenqi320@163.com>
2018-05-21 18:48:46 -0700

    accepted

--- 2_Add_Two_Number.py
+++ 2_Add_Two_Number.py
@@ -1,32 +1,47 @@
-tion for singly-linked list.
-class ListNode(object):
-     def __init__(self, x):
-        self.val = x
-        self.next = None
-
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
         a = 0
         b = 0
-        ListNode l = NULL
+        flag_a = 1
+        flag_b = 1
+        p  = ListNode(0)
+        p = l1
+        while(p.val == 0 and p.next != None):
+            flag_a = flag_a*10
+            p = p.next
+        p  = ListNode(0)
+        p = l2
+        while(p.val == 0 and p.next != None):
+            flag_b = flag_b*10
+            p = p.next
+            
         while l1 != None:
-           #print l1.val
            a = a*10+l1.val
            l1 = l1.next
         while l2 != None:
-           #print l2.val
            b = b*10 +l2.val
            l2 = l2.next
         #print a
         #print b
-        c = a+b
-        while c != 0:
-            l.val = c%10
-            l = l.next
-            c = c/10
-        return l
+        A=str(a)
+        B=str(b)
+        A = A[::-1]
+        B = B[::-1]
+        a = int(A)
+        b = int(B)
+        c =a*flag_a+b*flag_b
+        #print a ,b
+        ll = ListNode(0)
+        p  = ListNode(0)
+        p = ll
+        #print c % 10
+        while c >=10:
+            temp = ListNode(0)  
+            p.val = c % 10  
+            p.next = temp 
+            p = temp  
+            c = c / 10 
+            #print c
+            #print p.val
+        p.val = c %10
+        return ll
