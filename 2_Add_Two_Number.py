83f6540bfda8e52068e83222489178090daec167
xwqjojjr <xuwenqi320@163.com>
2018-05-21 18:56:20 -0700

    optimize

--- 2_Add_Two_Number.py
+++ 2_Add_Two_Number.py
@@ -1,35 +1,22 @@
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+    def Travel(self,l1):
         a = 0
-        b = 0
-        flag_a = 1
-        flag_b = 1
+        flag_a =1
         p  = ListNode(0)
         p = l1
         while(p.val == 0 and p.next != None):
             flag_a = flag_a*10
             p = p.next
-        p  = ListNode(0)
-        p = l2
-        while(p.val == 0 and p.next != None):
-            flag_b = flag_b*10
-            p = p.next
-            
         while l1 != None:
            a = a*10+l1.val
            l1 = l1.next
-        while l2 != None:
-           b = b*10 +l2.val
-           l2 = l2.next
-        #print a
-        #print b
         A=str(a)
-        B=str(b)
         A = A[::-1]
-        B = B[::-1]
         a = int(A)
-        b = int(B)
-        c =a*flag_a+b*flag_b
+        return a*flag_a
+    
+    def addTwoNumbers(self, l1, l2):
+        c = self.Travel(l1)+self.Travel(l2)
         #print a ,b
         ll = ListNode(0)
         p  = ListNode(0)
